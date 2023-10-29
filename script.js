0b2944846e1b80a2bea5c65d7c5811ae1977d7d9
T4t4ru <100865104+T4t4ru@users.noreply.github.com>
2023-10-29 14:46:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listeners for pixel clicks
+const pixels = document.querySelectorAll('.pixel');
+pixels.forEach(pixel => {
+    pixel.addEventListener('click', () => {
+        // Add your code to handle pixel click events here
+        // For example, change the pixel color or perform other actions
+    });
+});
